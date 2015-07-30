1a3b07b15b0968a4682398e0968c7135cbfdaa7d
Byran Wills-Heath <byran@adgico.co.uk>
2015-07-30 23:00:28 +0100

    Updated Agent Clang files to new naming scheme

--- agent-clang-3.6.2/build-docker-container.sh
+++ agent-clang-3.6.2/build-docker-container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker rmi adgico/teamcity_9.1_agent_clang_3.6.2
-docker build -t adgico/teamcity_9.1_agent_clang_3.6.2 .
+docker rmi adgico/teamcity-9.1-agent-clang-3.6.2
+docker build -t adgico/teamcity-9.1-agent-clang-3.6.2 .
--- agent-clang-3.6.2/create-teamcity-agent-clang-3.6.2.sh
+++ agent-clang-3.6.2/create-teamcity-agent-clang-3.6.2.sh
@@ -133,5 +133,5 @@ if [ "$AGENT_URL" != "" ] ; then
 	ENV="$ENV -e OWN_URL=$AGENT_URL"
 fi
 
-docker run --name=$NAME -p $PORT_MAPPING -d $ENV adgico/teamcity_9.1_agent_clang_3.6.2
+docker run --name=$NAME -p $PORT_MAPPING -d $ENV adgico/teamcity-9.1-agent-clang-3.6.2
 
